fa9dad3753eea2cf767da47e2dac13402b8489f7
mbd9441 <mdautntn@gmail.com>
2018-11-19 16:46:27 -0500

    Added improved UI for the orders page

--- HomePage.py
+++ HomePage.py
@@ -20,19 +20,26 @@ class homepage:
         self.subwindow.pack(fill=tkinter.X)
     
     def orders_customer(self):
-        self.displayorders=tkinter.Frame(self.subwindow)
-        self.displayorders.grid_columnconfigure(0, weight=1)
-        self.displayorders.grid_columnconfigure(1, weight=1)
-        self.displayorders.grid_columnconfigure(2, weight=1)
-        self.displayorders.grid_columnconfigure(3, weight=1)
-        self.tracking=tkinter.Label(self.displayorders,text="Tracking Number").grid(row=0,column=0)
-        self.deliverydate=tkinter.Label(self.displayorders,text="DeliveryDate").grid(row=0,column=1)
-        self.status=tkinter.Label(self.displayorders,text="Status").grid(row=0,column=2)
-        self.total=tkinter.Label(self.displayorders,text="Total").grid(row=0,column=3)
+        self.displayorders=tkinter.Frame(self.subwindow, relief=tkinter.SUNKEN)
+
+        self.scrollbar=tkinter.Scrollbar(self.displayorders)
+        self.scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+
+        self.scrollframe=tkinter.Listbox(self.displayorders, yscrollcommand=self.scrollbar.set)
+
+        self.scrollframe.grid_columnconfigure(0, weight=1)
+        self.scrollframe.grid_columnconfigure(1, weight=1)
+        self.scrollframe.grid_columnconfigure(2, weight=1)
+        self.scrollframe.grid_columnconfigure(3, weight=1)
+
+        self.tracking=tkinter.Label(self.scrollframe,text="Tracking Number").grid(row=0,column=0, sticky=tkinter.NSEW)
+        self.deliverydate=tkinter.Label(self.scrollframe,text="DeliveryDate").grid(row=0,column=1, sticky=tkinter.NSEW)
+        self.status=tkinter.Label(self.scrollframe,text="Status").grid(row=0,column=2, sticky=tkinter.NSEW)
+        self.total=tkinter.Label(self.scrollframe,text="Total").grid(row=0,column=3, sticky=tkinter.NSEW)
 
         orderresult = self.window.dbconnector.makequery("select tracking_number,status,date from shipping_order WHERE account_ID =(SELECT id from account where account.email ='%s');" % (self.window.userdict['email']))
         for i in range(0,len(orderresult)):
-            self.currentorder=tkinter.Frame(self.displayorders)
+            
             order = orderresult[i]
             print(order)
             orderdict={
@@ -46,13 +53,11 @@ class homepage:
             for key, value in orderdict.items():
                 if key == 'tracking':
                     print(value)
-                    self.trackingbutton=tkinter.Button(self.displayorders,text=value, command=lambda value=value:self.window.order_page(value))
-                    self.trackingbutton.grid(row=i+1, column=field, sticky=tkinter.EW)
+                    self.trackingbutton=tkinter.Button(self.scrollframe,text=value, command=lambda value=value:self.window.order_page(value))
+                    self.trackingbutton.grid(row=i+1, column=field, sticky=tkinter.NSEW)
                 else:
-                    self.currentfield=tkinter.Label(self.displayorders,text=value).grid(row=i+1, column=field)
+                    self.currentfield=tkinter.Label(self.scrollframe,text=value).grid(row=i+1, column=field, sticky=tkinter.NSEW)
                 field+=1
             
+        self.scrollframe.pack(fill=tkinter.X)
         self.displayorders.pack(fill=tkinter.X)
-
-    def orderbutton(self,orderid):
-        self.window.order_page(orderid)
